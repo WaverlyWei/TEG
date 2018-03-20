3ffb8317f9e534c08f54e90afc5ad2a337174b31
WaverlyWei <linqing_wei@berkeley.edu>
2018-03-19 21:57:53 -0700

    updated AUC results & sl3 fn

--- try_2017_acit.R
+++ try_2017_acit.R
@@ -16,6 +16,7 @@ library(R6)
 library(tidyverse)
 
 set.seed(12313197)
+options(digits = 4)
 #setwd("~/Dropbox (UC Berkeley Biostat)/hubbardlap/Mitch Cohen/Trauma and Coagulation (White Space Conflict)/ACIT/Data")
 dataf=read.dta13("ACIT_6Jan17_deidentified1.dta")
 plt0to6=as.numeric(dataf$icu_0to6h_plt_units>0)
@@ -74,21 +75,27 @@ pred.sets.nmes=list(standlabnmes,tegcknmes,tegcrtnmes,tegffnmes,rotemexnmes,rote
 prednmes=c("standLab","teg_ck","teg_crt","teg_ff","rotem_ex","rotem_intem","rotem_aptem","rotem_fibtem","rotem_all")
 ns=length(pred.sets)
 
+
+
 #change outcome to binary 
-Yt=dataf[,out[1]]
+Yt=dataf[,out[5]]
 n=length(Yt)
 Y=rep(NA,n)
 Y[Yt=="Dead"]=1
 Y[Yt=="Alive"]=0
-dataf[,out[1]]=Y
+dataf[,out[5]]=Y
 
 
-Y=dataf[,out[4]]
+# ======put together 
+auc_df = NULL
+for (i in 1:5){
+  for (j in 1:9){
+Y=dataf[,out[i]]
 include=is.na(Y)==F
 dat.t=dataf[include,]
 
-X = dat.t[,pred.sets[[1]]]
-Y = dat.t[,out[4]]
+X = dat.t[,pred.sets[[j]]]
+Y = dat.t[,out[i]]
 
 
 xp=dim(X)[2]
@@ -96,6 +103,8 @@ sum.na=function(x){sum(is.na(x))}
 sna=apply(X,2,sum.na)
 nmesX=names(X)
 
+
+# Set missing values delta 
 for(k in 1:xp){
   if(sna[k] > 0) {
     ix=as.numeric(is.na(X[,k])==F)
@@ -105,30 +114,140 @@ for(k in 1:xp){
 } 
 names(X)=nmesX    
 
-
-#this part works 
-acit_task <-make_sl3_Task(data = dat.t, outcome = out[4],covariates = pred.sets[[1]] )
+#  try this intermediate df
+temp = cbind(Y, X)
+acit_task <-make_sl3_Task(data = temp, outcome = "Y",covariates = colnames(temp)[!(colnames(temp) %in% "Y")],outcome_type = "binomial" )
 lrn1 = Lrnr_mean$new()
-lrn2 = Lrnr_glm$new()
+lrn2 = Lrnr_glmnet$new()
 lrn3 = Lrnr_randomForest$new()
-sl_lrn = Lrnr_sl$new(learners = list(lrn1,lrn3),metalearner = Lrnr_nnls$new())
+sl_lrn = Lrnr_sl$new(learners = list(lrn1,lrn2,lrn3),metalearner = Lrnr_nnls$new())
 acit_sl = sl_lrn$train(acit_task)
 acit_sl_pred=  acit_sl$predict()
 head(acit_sl_pred)
 risks = acit_sl$cv_risk(loss_squared_error)
+# try this =================AUC after (WORKS )
+predsY = acit_sl_pred
+YY = dat.t[,out[i]]
+ciout=ci.cvAUC(predsY, YY)
+auc = ciout$cvAUC
+ci_lo = ciout$ci[1]
+ci_hi = ciout$ci[2]
+se = ciout$se
+auc_df = rbind(auc_df,c(prednmes[j],out[i],auc,ci_lo,ci_hi,se))
+  }
+}
+auc_df = as.tibble(auc_df)
+names(auc_df) = c("PredSet","Outcome","AUC","CI_low","CI_high","SE")
+#  NEXT: PLOT AUC
+save(auc_df,file = "AUC.Rda")
+# ===================================================
+# ===============================================
+# ===============end of try =============
+is(iris)
+
+
+
+# ============ sl3  works / weird output =============
+acit_task <-make_sl3_Task(data = dat.t, outcome = out[4],covariates = pred.sets[[1]],outcome_type = "binomial" )
+lrn1 = Lrnr_mean$new()
+lrn2 = Lrnr_glmnet$new()
+lrn3 = Lrnr_randomForest$new()
+sl_lrn = Lrnr_sl$new(learners = list(lrn1,lrn2,lrn3),metalearner = Lrnr_nnls$new())
+acit_sl = sl_lrn$train(acit_task)
+acit_sl_pred=  acit_sl$predict()
+head(acit_sl_pred)
+risks = acit_sl$cv_risk(loss_squared_error)
+# =====================
+
+
+
+
 
 # prediction works now do AUC 
 ciout = ci.cvAUC(acit_sl_pred, labels = c(1:1645))
 
 
+
 ## Previous method with super learner 
 SL.library <- c("SL.glmnet", "SL.randomForest", "SL.mean","SL.rpartPrune")
-cl <- makeCluster(min(V,4))
-registerDoParallel(cl)
-n=length(Y)
-rownames(X)=paste("",1:n,sep="")
+#cl <- makeCluster(min(V,4))
+#registerDoParallel(cl)
+#n=length(Y)
+#rownames(X)=paste("",1:n,sep="")
+
 fit.test=CV.SuperLearner(Y, X, SL.library = SL.library, method = method.NNLS(), family = binomial(),V=V)
-rr=as.numeric(rownames(fit.test$SL.predict))
-YY=Y[rr]
+##### ROC Curve
+fld=fit.test$fold
+# YY / binary / labels 
+YY=fit.test$Y
 predsY=fit.test$SL.predict
-ciout = ci.cvAUC(predsY, labels = c(1:1671))
+n=length(predsY)
+fold=rep(NA,n)
+for(k in 1:V) {
+  ii=unlist(fld[k])
+  fold[ii]=k
+}
+ciout=ci.cvAUC(predsY, YY, folds = fold)
+txt=paste("AUC = ",round(ciout$cvAUC,2),",  95% CI = ",round(ciout$ci[1],2),"-",round(ciout$ci[2],2),sep="")
+
+
+#### Compare via ROC plots
+preds=fit.test$SL.predict
+# make prefiction object 
+pred <- prediction(preds,Y)
+perf1 <- performance(pred, "sens", "spec")
+filn=paste(filepre,".pdf",sep="")
+pdf(filn)
+titl=paste("ROC with CV AUC:  ",outc," vs ",predn,sep="")
+plot(1-slot(perf1,"x.values")[[1]],slot(perf1,"y.values")[[1]],type="s",xlab="1-Specificity",ylab="Sensitivity"
+     )
+text(0.75,0.4,txt)
+abline(0,1)
+dev.off()
+
+
+dat.tmp2 = data.frame(Y,X)
+fit.rpart=rpart(Y ~ ., data =dat.tmp2, control = rpart.control(cp = 0.001, minsplit = 20, 
+                                                               xval = 20, maxdepth = 1, minbucket = 10), method = "class")
+plot(fit.rpart)
+
+printcp(fit.rpart)
+plotcp(fit.rpart)
+plot(fit.rpart, uniform=TRUE, 
+     main="Classification Tree for Kyphosis")
+
+
+
+
+
+# ==========================Haven't figured out yet 
+
+xsplt=fit.rpart$splits
+varsplt = NULL
+if(is.null(xsplt)) {
+  xs=c(xs,NA)
+  oper=c(oper,"")
+}
+if(is.null(xsplt)==F) {
+  xname=rownames(xsplt)[1]  
+  xsplt=xsplt[1,"index"]	
+  x1=dat.tmp2[,xname]
+  ix1=as.numeric(x1>xsplt)
+  tt=table(dat.tmp2[,1],ix1)
+  y1=tt[2,1]/sum(tt[,1])
+  y2=tt[2,2]/sum(tt[,2])
+  optmp = ifelse(y1>y2,"<",">")
+  xs=c(xs,xsplt)
+  oper=c(oper,optmp)
+  varsplt=c(varsplt,xname)
+  pp=predict(fit.rpart,type="class")
+  dat.tmp2=dat.tmp2[pp==0,names(dat.tmp2)!=xname]
+}
+nv=length(varsplt)
+if(nv = 0) {
+  setsx=c(setsx,predn)
+  outnames=c(outnames,outc)
+  varn=c(varn,"none")
+}
+
+dd=data.frame(outnames,setsx=setsx,varnames=varn,cut=xs,oper=oper)
